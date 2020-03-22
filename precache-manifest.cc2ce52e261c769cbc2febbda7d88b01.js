126277125418091fc36592dda3d88ae3781bac5d
Alexey <valnistuy@gmail.com>
2020-03-22 16:43:41 +0200

    Updates

---
+++ precache-manifest.cc2ce52e261c769cbc2febbda7d88b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d30ccd2a09cc9460d3a09636fba391f",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "ea6e60152232ee5d8989",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0ea86114bf6529774652",
+    "url": "/goit-react-hw-02-feedback/static/js/2.800e3ced.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.800e3ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6e60152232ee5d8989",
+    "url": "/goit-react-hw-02-feedback/static/js/main.d702ab8b.chunk.js"
+  },
+  {
+    "revision": "f59aa7dbc0213e80a103",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb39d68.js"
+  }
+]);
